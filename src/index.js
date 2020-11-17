fff36c0f8699204f92e3f8818b40321ce885bfe0
Dani Lucaci <danilucaci@gmail.com>
2020-11-17 16:50:26 +0100

    remove comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import users from "./__mocks__/users";
 
 function thisFunctionNameMakesNoSense() {
-  // TODO: add issue to fetch from a real API - now Dani
   users.forEach((user) => {
     const currentTableNode = document.createElement("tr");
 
